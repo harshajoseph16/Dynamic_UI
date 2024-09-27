37e3603da946e0f11017f88ab7ce0c2f8b1b70ac
Harsha Joseph <harshajoseph16@gmail.com>
2024-09-27 14:34:48 +0530

    home page

---
+++ src/assets/Componets/Nav/index.jsx
@@ -0,0 +1,11 @@
+import 'nav.css'
+
+
+Nav = ()=> {
+    <div className="top-head">
+dfsfs
+    </div>
+}
+
+
+export default Nav
---
+++ src/assets/Pages/Home/index.jsx
@@ -0,0 +1,10 @@
+import './home.css'
+const Home = () => {
+    <div className="wrapper">
+        
+
+        <Navbar/>
+    </div>
+}
+
+export default Home
